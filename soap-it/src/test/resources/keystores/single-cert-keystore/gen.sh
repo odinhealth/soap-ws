43d9c1e883b065d5822940d9681399ac95c320b2
michael.fowke <michael.fowke@odinhealth.co.nz>
2021-12-06 12:38:59 +1300

    Remove client related libraries

--- soap-it/src/test/resources/keystores/single-cert-keystore/gen.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-keytool -genkey -keystore .keystore_1 -alias john -storepass changeit -keypass changeit -dname "CN=John Smith, OU=Development, O=Standard Supplies Inc., L=Anytown, S=North Carolina, C=US"
-keytool -genkey -keystore .keystore_2 -alias george -storepass changeit -keypass changeit -dname "CN=George Kowalski, OU=Development, O=Standard Supplies Inc., L=Anytown, S=North Carolina, C=US"
-
